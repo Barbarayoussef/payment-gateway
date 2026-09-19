107d352b6197fd926b9b66f7e6327e66922a66ef
Barbarayoussef <barayoussef12@yahoo.com>
2026-09-19 13:52:48 +0300

    Adding pay api request in PaymentController

---
+++ src/main/java/com/bank/payment_gateway/controller/PaymentController.java
@@ -0,0 +1,21 @@
+package com.bank.payment_gateway.controller;
+
+import com.bank.payment_gateway.model.PaymentRequest;
+import com.bank.payment_gateway.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/payment")
+public class PaymentController {
+    @Autowired
+    private PaymentService paymentService;
+
+    @PostMapping("/pay")
+    public String makePayment(@RequestBody PaymentRequest paymentRequest){
+        return paymentService.processPayment(paymentRequest);
+    }
+}
